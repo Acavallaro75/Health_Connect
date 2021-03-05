daf31b97097624d5a02f7a0c27cd9a563b086f57
Acavallaro75 <andrew.cavallarojr@gmail.com>
2021-03-05 14:28:40 -0500

    Got project running and database setup

--- src/health/Health.java
+++ src/health/Health.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package health;
 
 import java.sql.Connection;
@@ -13,23 +7,22 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 public class Health {
-
-  /** @param args the command line arguments */
   public static void main(String[] args) {
     // TODO code application logic here
 
-    @SuppressWarnings("UnusedAssignment")
-    // test connection
-    Connection connection = null;
+    Connection connection;
     try {
       Class.forName("org.sqlite.JDBC");
-      connection =
-          DriverManager.getConnection(
-              "jdbc:sqlite:C:\\Users\\Andrea\\Documents\\NetBeansProjects\\health.sqlite");
+      connection = DriverManager.getConnection("jdbc:sqlite:Health_Connect_DB");
       JOptionPane.showMessageDialog(null, "Connected");
       Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery("SELECT * FROM Patient");
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM PATIENT");
 
       while (resultSet.next()) {
         System.out.println("Username = " + resultSet.getString("Username"));
@@ -42,6 +35,7 @@ public class Health {
         System.out.println("Username = " + resultSet.getString("Username"));
         System.out.println("Password = " + resultSet.getString("Password"));
       }
+
       // open login page
       NewJFrame jFrame = new NewJFrame();
       jFrame.setVisible(true);
